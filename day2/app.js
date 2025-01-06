3517943991d34e3fcb41ac5dde31cf8369488405
Bhargavmarre <bhargavmarre@gmail.com>
2025-01-06 22:02:28 +0530

    pushing into repo

---
+++ day2/app.js
@@ -0,0 +1,27 @@
+//string
+let str1="double quotes";
+console.log(str1,typeof(str1));
+let str2='single quotes';
+console.log(str2,typeof(str2));
+let str3=`template literals`
+console.log(str3,typeof(str3))
+
+//boolean
+let bool1=true
+console.log(bool1,typeof(bool1))
+let bool2=false
+console.log(bool2,typeof(bool2))
+
+//numbers
+let num=2
+console.log(num,typeof(num))
+let num2=10e2
+console.log(num2,typeof(num2))
+
+//undefined
+let a;
+console.log(a,typeof(a))
+
+//null
+let x=null
+console.log(x,typeof(x))
