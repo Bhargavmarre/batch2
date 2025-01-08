6a55f431bfef62758833a7727d5ced58cf42b415
Bhargavmarre <bhargavmarre@gmail.com>
2025-01-08 13:20:58 +0530

    new files added

---
+++ looping.js
@@ -0,0 +1,21 @@
+//for(let i=1;i<=5;i++){
+//    console.log("javascript")
+//}
+
+//for (let i=0;i<=100;i++){
+//    if(i%2 ==0){
+//        console.log(i +"is a even number");  
+ //   }
+ //   else{
+//        console.log(i +"is a odd number");
+        
+//    }
+    
+//}
+
+
+let mul=1;
+let num=Number(prompt("enter a number: "))
+for(let i=1;i<=5;i++){
+    
+}
